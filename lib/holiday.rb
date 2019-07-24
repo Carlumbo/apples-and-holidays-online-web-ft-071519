87b52da31e67a7ab5ef32123909c5577257adb2a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-24 01:39:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, item|
       holiday if item.include?("BBQ")
-      holiday
     end 
   end
 end 
